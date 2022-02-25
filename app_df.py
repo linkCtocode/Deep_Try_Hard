8b990206b42ffa2bcc727a03ba33fcb52162c903
yassine laghrari <laghrari.yassine@gmail.com>
2022-02-25 10:14:33 +0100

    small changes

--- app_df.py
+++ app_df.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 
 def app():
-    st.markdown("""# This is a header
-    ## This is a sub header
-    This is text""")
 
     df = pd.DataFrame({
             'first column': list(range(1, 11)),
